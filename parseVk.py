99bbbb926ee5797ac47888e8467eaa1f1b09c167
CheekyRyawNow <CheekyRyawNow@yandex.ru>
2021-10-26 08:43:29 +0400

    1) Add tagging 2) Read csv and build a right vk object required 3) Rewrite code required

--- parseVk.py
+++ parseVk.py
@@ -12,7 +12,7 @@ PASSWORD = os.getenv('PASSWORD')
 ID_RANGE_START = 1000000
 ID_RANGE_END = 50000000
 QUERY_CONSTRAINT = 1000
-REQUESTS_NUMBER = 10
+REQUESTS_NUMBER = 5
 COMMON_FIELDS = [
     'bdate',
     'sex',
@@ -26,8 +26,11 @@ PUBLIC_FIELDS = [
     'games',
     'books'
 ]
+TAG = ['tag']
 PRIVATE_USERS_FILENAME = 'private_users.csv'
 PUBLIC_USERS_FILENAME = 'public_users.csv'
+TAGGED_PRIVATE_USERS_FILENAME = 'private_users_tagged.csv'
+TAGGED_PUBLIC_USERS_FILENAME = 'public_users_tagged.csv'
 
 
 def connect_vk():
@@ -40,7 +43,7 @@ def connect_vk():
 def parse_users(vk, requests_number):
     private_users = []
     public_users = []
-    for i in range(1):
+    for i in range(requests_number):
         ids = []
         for i in range(QUERY_CONSTRAINT):
             ids.append(np.random.randint(ID_RANGE_START, ID_RANGE_END))
@@ -51,53 +54,46 @@ def parse_users(vk, requests_number):
                     private_users.append(user)
                 else:
                     public_users.append(user)
-    # return private_users, public_users
-    save_users_csv(private_users, COMMON_FIELDS, PRIVATE_USERS_FILENAME)
-    save_users_csv(public_users, COMMON_FIELDS + PUBLIC_FIELDS, PUBLIC_USERS_FILENAME)
+    return private_users, public_users
 
 
-def save_users_csv(users, fields, path_to_save):
-    # try:
-    #     dataFrame = pd.read_csv(path_to_save)
-    #     data = dataFrame.to_numpy()
-    #     data = np.delete(data, 0, 1)
-    # except FileNotFoundError:
-    #     dataFrame = pd.DataFrame(columns=fields)
-    #     data = dataFrame.to_numpy()
+def save_parsed_users(users, fields, path_to_save):
     data, dataFrame = read_csv(path_to_save, fields)
     for user in users:
         if user['id'] not in data[:, 0]:
             new_row = []
+            if 'bdate' in user and len(user['bdate']) < 8:
+                user['bdate'] = None
             for field in fields:
                 if field not in user or not user[field]:
                     user[field] = None
                 new_row.append(user[field])
-            if len(user['bdate']) != 10:
-                user['bdate'] == None
             data = np.vstack([data, new_row])
-    # save_csv() 
-    dataFrame = pd.DataFrame(data, columns=fields)
-    dataFrame.to_csv(path_to_save)
+    save_csv(path_to_save, fields, data) 
+    return users
 
  
-def read_csv(path, fields):
+def read_csv(path_to_read, fields):
     try:
-        dataFrame = pd.read_csv(path)
+        dataFrame = pd.read_csv(path_to_read)
         data = dataFrame.to_numpy()
         data = np.delete(data, 0, 1)
     except FileNotFoundError:
         dataFrame = pd.DataFrame(columns=fields)
         data = dataFrame.to_numpy()
+    # return data, dataFrame
     return data, dataFrame
 
 
-def save_csv(path, fields):
-    raise Exception()
+def save_csv(path_to_save, fields, data):
+    dataFrame = pd.DataFrame(data, columns=fields)
+    dataFrame.to_csv(path_to_save)
 
 
 def tag_users(users):
     user_age = 0
     for user in users:
+        user['tag'] = []
         if user['is_closed']:
             user['tag'].append('Security')
         else:
@@ -109,10 +105,12 @@ def tag_users(users):
                 user['tag'].append('Motherhood')
             if user_age < 40:
                 user['tag'].append('Games')
-            if user['last_seen']['platform'] != 7:
-                user['tag'].append('Mobile')
-            else:
-                user['tag'].append('Desktop')
+            if user['last_seen'] != None:
+                if 'platform' in user['last_seen']:
+                    if user['last_seen']['platform'] != 7:
+                        user['tag'].append('Mobile')
+                    else:
+                        user['tag'].append('Desktop')
             if user_age > 60:
                 user['tag'].append('Old_people')
             if user_age > 35:
@@ -126,6 +124,18 @@ def tag_users(users):
                 user['tag'].append('Love_in')
             if user['books'] != None:
                 user['tag'].append('Books')
+    return users
+
+
+def save_tagged_users(users, fields, path_to_save):
+    data, dataFrame = read_csv(path_to_save, fields)
+    for user in users:
+        if user['id'] not in data[:, 0]:
+            new_row = []
+            for field in fields:
+                new_row.append(user[field])
+            data = np.vstack([data, new_row])
+    save_csv(path_to_save, fields, data)
 
 
 # DD.MM.YYYY string
@@ -138,11 +148,10 @@ def calculate_age(bdate):
 if __name__ == '__main__':
     vk = connect_vk()
     private_users, public_users = parse_users(vk, REQUESTS_NUMBER)
-    # save_users_csv(private_users, COMMON_FIELDS.append('tag'), PRIVATE_USERS_FILENAME)
-    # save_users_csv(public_users, (COMMON_FIELDS + PUBLIC_FIELDS).append('tag'), PUBLIC_USERS_FILENAME)
-    
-    # private_users_tagged = tag_users(private_users)
-    # public_users_tagged = tag_users(public_users)
-    # save_users_csv(private_users_tagged)
-    # save_users_csv(public_users_tagged)
+    private_users = save_parsed_users(private_users, COMMON_FIELDS + TAG, PRIVATE_USERS_FILENAME)
+    public_users = save_parsed_users(public_users, COMMON_FIELDS + PUBLIC_FIELDS + TAG, PUBLIC_USERS_FILENAME)
 
+    private_users_tagged = tag_users(private_users)
+    public_users_tagged = tag_users(public_users)
+    save_tagged_users(private_users_tagged, COMMON_FIELDS + TAG,TAGGED_PRIVATE_USERS_FILENAME)
+    save_tagged_users(public_users_tagged, COMMON_FIELDS + PUBLIC_FIELDS + TAG, TAGGED_PUBLIC_USERS_FILENAME)
