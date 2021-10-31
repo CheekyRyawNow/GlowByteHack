241cc1c709beca96626b9006d521993cbbff44db
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2021-10-31 13:19:46 +0400

    Added constant file

---
+++ src/Constants.py
@@ -0,0 +1,18 @@
+fields = [
+    'id',
+    'first_name',
+    'last_name',
+    'bdate',
+    'sex',
+    'last_seen',
+    'can_write_private_message',
+    'country',
+    'city',
+    'relation'
+]
+
+id_range_start = 1000000
+id_range_end = 50000000
+requests_number = 10
+
+users_filename = '../users.csv'
--- src/parseVk.py
+++ src/parseVk.py
@@ -1,27 +1,16 @@
 from VKService import VKService
 from Filter import Filter
 from FileService import FileService
+import Constants
 
-FIELDS = [
-    'id',
-    'first_name',
-    'last_name',
-    'bdate',
-    'sex',
-    'last_seen',
-    'can_write_private_message',
-    'country',
-    'city',
-    'relation'
-]
 
 if __name__ == '__main__':
-    vk = VKService(FIELDS, 1000000, 50000000, 2)
+    vk = VKService(Constants.fields, Constants.id_range_start, Constants.id_range_end, Constants.requests_number)
     users = vk.get_users()
 
-    filter = Filter(users, FIELDS)
+    filter = Filter(users, Constants.fields)
     users_filtered = filter.get_filtered_users()
 
-    file_service = FileService('../users.csv')
-    file_service.save_users(users_filtered, file_service._users_filename, FIELDS)
+    file_service = FileService(Constants.users_filename)
+    file_service.save_users(users_filtered, file_service._users_filename, Constants.fields)
     users = file_service.read_users(file_service._users_filename)
