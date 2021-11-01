a886912bfa8ac1054c74331b480a42cc10890810
CheekyRyawNow <CheekyRyawNow@yandex.ru>
2021-11-01 11:19:49 +0400

    More efficient objects use

--- src/parseVk.py
+++ src/parseVk.py
@@ -6,16 +6,19 @@ import Constants
 
 if __name__ == '__main__':
     vk = VKService(Constants.fields, Constants.id_range_start, Constants.id_range_end, Constants.requests_number)
+    file_service = FileService(Constants.users_filename)
+    filter = Filter(None, Constants.fields)
     for i in range(Constants.users_files_number): 
         users = vk.get_users()
 
-        filter = Filter(users, Constants.fields)
+        filter._Filter__users = users
         users_filtered = filter.get_filtered_users()
 
-        file_service = FileService(Constants.users_filename + str(i))
+        file_service._users_filename = Constants.users_filename + str(i)
         file_service.save_users(users_filtered, file_service._users_filename, Constants.fields)
-    
+   
     for i in range(Constants.users_files_number):
-        file_service = FileService(Constants.users_filename + str(i))
+        file_service._users_filename = Constants.users_filename + str(i)
         users = file_service.read_users(file_service._users_filename)
+
     print()
